56062fda0379069b8367c524d7d66bd8de26b6eb
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-10-10 17:46:54 +0530

    done now

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ function hideMenu() {
 // }
 
 // setInterval(updateNotice, 1000);
+
+
+// Function to display the current date and time
+function displayCurrentDateTime() {
+    // Get the current date and time
+    const currentDateTime = new Date();
+
+    // Format the date as desired (e.g., "Month Day, Year")
+    const dateOptions = { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' };
+    const formattedDate = currentDateTime.toLocaleDateString('en-US', dateOptions);
+
+    // Format the time as desired (e.g., "HH:MM:SS AM/PM")
+    const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
+    const formattedTime = currentDateTime.toLocaleTimeString('en-US', timeOptions);
+
+    // Update the content of the "currentDate" and "currentTime" divs
+    document.getElementById('currentDate').textContent = `${formattedDate}`;
+    document.getElementById('currentTime').textContent = `Time - ${formattedTime}`;
+}
+
+// Call the function to display the current date and time when the page loads
+window.onload = displayCurrentDateTime;
